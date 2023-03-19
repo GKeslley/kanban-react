f79672d0e8579d4d7d67af939fcef0a433781945
guilherme <guilhermekeslley123@gmail.com>
2023-03-19 10:59:07 -0300

    Componente dos cards

---
+++ src/Componentes/ToDo/Card.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+const Card = ({ card, global, index, listIndex, move, list }) => {
+  const { id, dados, subtasks } = card;
+  const ref = React.useRef();
+
+  const handleClick = ({ id, dados, subtasks }) => {
+    global.setShowDados({ id, dados, subtasks });
+    setTimeout(() => {
+      global.setVisible(true);
+    }, 10);
+  };
+
+  const [{ isDragging }, dragRef] = useDrag({
+    type: 'CARD',
+    item: { id, index, listIndex },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor) {
+      const draggedDrop = item.index;
+      const targetIndex = index;
+
+      if (draggedDrop === targetIndex || listIndex !== item.listIndex) return;
+
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+
+      if (draggedDrop < targetIndex && draggedTop < targetCenter) return null;
+      if (draggedDrop > targetIndex && draggedTop > targetCenter) return null;
+
+      move(draggedDrop, targetIndex, dados.status);
+      item.index = targetIndex;
+    },
+  });
+
+  dragRef(dropRef(ref));
+  return (
+    <div>
+      <div
+        className="task"
+        onClick={() => handleClick({ id, dados, subtasks })}
+        key={id}
+        id={id}
+        ref={ref}
+      >
+        <span>{dados.title}</span>
+        <span>
+          {subtasks ? Object.values(subtasks).filter(({ mark }) => mark).length : 0} of{' '}
+          {subtasks ? Object.values(subtasks).filter(({ value }) => value).length : 0}{' '}
+          subtasks
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
